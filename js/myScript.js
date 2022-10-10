39a06f42936ea69787c0ecf953012129dd53b31a
BillyCWood <billyc.wood4@gmail.com>
2022-10-10 09:33:06 -0600

    modified:   creation.html 	modified:   css/index.css 	modified:   history.html 	modified:   index.html 	new file:   js/myScript.js

---
+++ js/myScript.js
@@ -0,0 +1,6 @@
+function navBarResponse(){
+    var x = document.getElementById("topNav");
+    if(x.className === "topNav"){
+        x.className += " responsive";
+    }else{x.className = "topNav";}
+}
